80916db36559fd9fa422ed57a9f65b1704adf25d
LeVirus <1203896@gmail.com>
2019-07-15 20:57:52 +0200

    Add shader class.

---
+++ Nietsneflow3d/OpenGLUtils/Shader.cpp
@@ -0,0 +1,120 @@
+#include <glad.h>
+#include <cassert>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include "Shader.hpp"
+
+//===================================================================
+Shader::Shader()
+{
+
+}
+
+//===================================================================
+Shader::Shader(const std::string &vertexPath, const std::string &fragmentPath)
+{
+    setSources(vertexPath, fragmentPath);
+}
+
+//===================================================================
+void Shader::setSources(const std::string &vertexPath,
+                        const std::string &fragmentPath)
+{
+    std::ifstream vertexShaderFile, fragmentShaderFile;
+    vertexShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+    fragmentShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+    try
+    {
+        vertexShaderFile.open(vertexPath);
+        fragmentShaderFile.open(fragmentPath);
+        std::stringstream vertexShaderStream, fragmentShaderStream;
+        vertexShaderStream << vertexShaderFile.rdbuf();
+        fragmentShaderStream << vertexShaderFile.rdbuf();
+        m_vertexSource = vertexShaderStream.str();
+        m_fragmentSource = fragmentShaderStream.str();
+        vertexShaderFile.close();
+        fragmentShaderFile.close();
+    }
+    catch(std::ifstream::failure e)
+    {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ " << std::endl;
+    }
+
+}
+
+//===================================================================
+void Shader::use()
+{
+    glUseProgram(m_shaderProgram);
+}
+
+//===================================================================
+void Shader::generateShader()
+{
+    bool success;
+    success = generateVertexShader();
+    assert(success && "Error while generating vertex shader");
+    success = generateFragmentShader();
+    assert(success && "Error while generating fragment shader");
+}
+
+//===================================================================
+void Shader::linkShader()
+{
+    // link shaders
+    m_shaderProgram = glCreateProgram();
+    glAttachShader(m_shaderProgram, m_vertexShader);
+    glAttachShader(m_shaderProgram, m_fragmentShader);
+    glLinkProgram(m_shaderProgram);
+    char infoLog[512];
+    int success;
+    // check for linking errors
+    glGetProgramiv(m_shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(m_shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        assert("ERROR::SHADER::PROGRAM::LINKING_FAILED");
+    }
+    glDeleteShader(m_vertexShader);
+    glDeleteShader(m_fragmentShader);
+}
+
+//===================================================================
+bool Shader::generateVertexShader()
+{
+    m_vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const char *vertSource = m_vertexSource.c_str();
+    glShaderSource(m_vertexShader, 1, &vertSource, NULL);
+    glCompileShader(m_vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(m_vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(m_vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    return success;
+}
+
+//===================================================================
+bool Shader::generateFragmentShader()
+{
+    m_fragmentShader= glCreateShader(GL_FRAGMENT_SHADER);
+    const char *fragSource = m_fragmentSource.c_str();
+    glShaderSource(m_fragmentShader, 1, &fragSource, NULL);
+    glCompileShader(m_fragmentShader);
+    char infoLog[512];
+    int success;
+    // check for shader compile errors
+    glGetShaderiv(m_fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(m_fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    return success;
+}
---
+++ Nietsneflow3d/OpenGLUtils/Shader.hpp
@@ -0,0 +1,30 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <string>
+
+class Shader
+{
+private:
+    std::string m_vertexSource, m_fragmentSource;
+    int m_vertexShader, m_fragmentShader, m_shaderProgram;
+private:
+    bool generateVertexShader();
+    bool generateFragmentShader();
+public:
+    Shader();
+
+    /**
+     * @brief Shader
+     * @param vertexPath Path to vertex shaders sources.
+     * @param fragmentPath Path to fragment shaders sources.
+     */
+    Shader(const std::string &vertexPath, const std::string &fragmentPath);
+    void generateShader();
+    void linkShader();
+    bool compileSource()const;
+    void setSources(const std::string &vertexPath, const std::string &fragmentPath);
+    void use();
+};
+
+#endif // SHADER_H
