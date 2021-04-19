b26e3ad710141449d1732bdcfa3e200f9db23a00
LeVirus <1203896@gmail.com>
2021-04-19 21:57:32 +0200

    Add ini.

--- Nietsneflow3d/FontData.cpp
+++ Nietsneflow3d/FontData.cpp
@@ -27,7 +27,6 @@ VectSpriteDataRef_t FontData::getWriteData(const std::string &str, uint32_t &num
     {
         return {};
     }
-    bool textureNumDefined = false;
     VectSpriteDataRef_t vect;
     vect.reserve(str.size());
     std::map<char, SpriteData>::const_iterator it;
@@ -37,10 +36,9 @@ VectSpriteDataRef_t FontData::getWriteData(const std::string &str, uint32_t &num
         if(it != m_mapFontData.end())
         {
             vect.emplace_back(const_cast<SpriteData&>(it->second));
-            if(!textureNumDefined)
+            if(i == 0)
             {
                 numTexture = it->second.m_textureNum;
-                textureNumDefined = true;
             }
         }
     }
