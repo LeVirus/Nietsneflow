1d358a58423731be4198a3dee41d26cdf761715b
Cyril <1203896@gmail.com>
2021-12-15 10:16:53 +0100

    Opti.

--- Nietsneflow3d/OpenGLUtils/VerticesData.cpp
+++ Nietsneflow3d/OpenGLUtils/VerticesData.cpp
@@ -133,8 +133,16 @@ float VerticesData::loadRaycastingEntity(const SpriteTextureComponent &spriteCom
     float diffTotalTexturePos = (spriteComp.m_spriteData->m_texturePosVertex[1].first - spriteComp.m_spriteData->m_texturePosVertex[0].first);
     for(uint32_t i = 0; i < raycastingData.size(); ++i)
     {
-        lateralGLPosA = raycastingData[i].m_lateral * m_raycastStep - 1.0f;
-        lateralGLPosB = lateralGLPosA + m_raycastStep;
+        if(i > 0 && raycastingData[i].m_lateral == raycastingData[i - 1].m_lateral - 1)
+        {
+            lateralGLPosA += m_raycastStep;
+            lateralGLPosB += m_raycastStep;
+        }
+        else
+        {
+            lateralGLPosA = raycastingData[i].m_lateral * m_raycastStep - 1.0f;
+            lateralGLPosB = lateralGLPosA + m_raycastStep;
+        }
         verticalPos = RAYCAST_VERTICAL_SIZE / (raycastingData[i].m_distance / LEVEL_TILE_SIZE_PX);
         lateralText = spriteComp.m_spriteData->m_texturePosVertex[0].first +
                 (raycastingData[i].m_texturePos / LEVEL_TILE_SIZE_PX) * diffTotalTexturePos;
