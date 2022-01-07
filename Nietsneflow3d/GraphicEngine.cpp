6ac34554848cd7d1de164f2f4cfb634be6845846
Cyril <1203896@gmail.com>
2022-01-07 14:28:56 +0100

    Prevent manual window resize.

--- Nietsneflow3d/GraphicEngine.cpp
+++ Nietsneflow3d/GraphicEngine.cpp
@@ -310,6 +310,7 @@ void GraphicEngine::initGLWindow()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
 #endif
