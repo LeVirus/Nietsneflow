13194ba5e97c6389b14f0fee1656ed70d14523dc
Cyril <1203896@gmail.com>
2022-04-13 07:36:12 +0200

    Remove precentage displayed when die.

--- Nietsneflow3d/GraphicEngine.cpp
+++ Nietsneflow3d/GraphicEngine.cpp
@@ -516,6 +516,10 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 //===================================================================
 std::string getEndLevelMenuStr(const std::tuple<const PlayerConfComponent*, uint32_t, uint32_t> &endLevelData)
 {
+    if(std::get<0>(endLevelData)->m_life == 0)
+    {
+        return "\\PRESS ENTER TO RESTART";
+    }
     float enemiesKilledPercent =
             (!std::get<0>(endLevelData)->m_enemiesKilled || std::get<2>(endLevelData) == 0) ? EPSILON_FLOAT :
                 static_cast<float>(*std::get<0>(endLevelData)->m_enemiesKilled) /
