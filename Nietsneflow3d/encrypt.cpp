188bf7a416d1ce060a1edd8fed4e6489c44d8346
LeVirus <1203896@gmail.com>
2023-05-14 16:38:44 +0200

    Add new pictures and items.

--- Nietsneflow3d/encrypt.cpp
+++ Nietsneflow3d/encrypt.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 
 //ENCRYPT_KEY_CONF_FILE
-//const uint32_t KEY = 42;
+const uint32_t KEY = 42;
 //STANDARD LEVEL
 //const uint32_t KEY = 17;
 //CUSTOM LEVEL
-const uint32_t KEY = 52;
+//const uint32_t KEY = 52;
 
 //===================================================================
 std::string decrypt(const std::string &str, uint32_t key)
@@ -40,12 +40,13 @@ int main(int argc, char *argv[])
 		//	return -1;
 		//}
 		//std::string path = argv[1];
-		std::ifstream inStream("./Ressources/CustomLevels/ff.clvl");
+//		std::ifstream inStream("./Ressources/CustomLevels/ff.clvl");
 		//std::ifstream inStream("./Ressources/fontStandard.ini.base");
 		//std::ifstream inStream("./Ressources/fontStandard.ini.base");
 		//std::cout << "./Ressources/" + path + "/level.ini.dd \n";
 		//std::ifstream inStream("./Ressources/" + path + "/level.ini.dec");
-//		std::ifstream inStream("./Ressources/standardData.ini.base");
+		//std::ifstream inStream("./Ressources/standardData.ini.base");
+		std::ifstream inStream("./Ressources/pictureData.ini.base");
 		if(inStream.fail())
 		{
 				std::cout << "Fail\n";
@@ -55,12 +56,13 @@ int main(int argc, char *argv[])
 		std::ostringstream ostringStream;
 		ostringStream << inStream.rdbuf();
 		inStream.close();
-		std::string dataString = decrypt(ostringStream.str(), KEY);
+		std::string dataString = encrypt(ostringStream.str(), KEY);
 
 		//std::ofstream outStream("./Ressources/fontData.ini");
-		std::ofstream outStream("./Ressources/CustomLevels/FabbDec");
+		//std::ofstream outStream("./Ressources/CustomLevels/FabbDec");
 		//std::ofstream outStream("./Ressources/" + path + "/level.ini");
 		//std::ofstream outStream("./Ressources/standardData.ini");
+		std::ofstream outStream("./Ressources/pictureData.ini");
 		outStream << dataString;
 		outStream.close();
 		std::cout << "OK\n";
