c8596bb839e0971a6712c56cef019f0a683de86e
Cyril <1203896@gmail.com>
2023-10-01 15:53:32 +0200

    Fix level issues.

--- Nietsneflow3d/Ressources/install.sh
+++ Nietsneflow3d/Ressources/install.sh
@@ -1,3 +1,4 @@
 cp Nietsneflow3d/Lib/lib* /usr/local/lib/
+cp bin/Arnihs /usr/local/bin/
 cp -r Nietsneflow3d/Ressources/ $HOME/.local/share/Arnihs
 cp Arnihs.desktop $HOME/.local/share/applications
