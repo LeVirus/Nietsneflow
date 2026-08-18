d0c903059159b3a322df6b9849946a8ff4aef47b
LeVirus <1203896@gmail.com>
2026-08-18 13:36:47 +0200

    Fix player shot coherence.

--- Nietsneflow3d/ECS/Systems/IASystem.cpp
+++ Nietsneflow3d/ECS/Systems/IASystem.cpp
@@ -440,7 +440,7 @@ void IASystem::confVisibleShoot(std::vector<uint32_t> &visibleShots, const PairF
     mapComp.m_coord = *coord;
     mapComp.m_absoluteMapPositionPX = point;
     m_mainEngine->addEntityToZone(visibleShots[currentShot], mapComp.m_coord);
-    moveElementFromAngle(LEVEL_THIRD_TILE_SIZE_PX , getRadiantAngle(degreeAngle),
+    moveElementFromAngle(LEVEL_THIRD_TILE_SIZE_PX , getRadiantAngle(degreeAngle - 20.0f),
                          mapComp.m_absoluteMapPositionPX);
     ammoMoveComp.m_degreeOrientation = degreeAngle;
     ammoMoveComp.m_currentDegreeMoveDirection = degreeAngle;
